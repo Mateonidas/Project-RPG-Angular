72eddaf6dba683b99c182038655f00333a282206
mateonidas <teus.polec.other@gmail.com>
2021-09-12 10:03:57 +0200

    feat: add validation to save roll dialog window

--- src/app/dialog-window/save-roll-dialog-window/save-roll-dialog-window.component.ts
+++ src/app/dialog-window/save-roll-dialog-window/save-roll-dialog-window.component.ts
@@ -1,6 +1,6 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
-import {FormControl, FormGroup} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-save-roll-dialog-window',
@@ -21,14 +21,21 @@ export class SaveRollDialogWindowComponent implements OnInit {
 
   initForm() {
     this.saveForm = new FormGroup({
-      'roll': new FormControl(null),
-      'modifier': new FormControl(0),
+      'roll': new FormControl(null, [Validators.required]),
+      'modifier': new FormControl(0, [Validators.required]),
     })
   }
 
   onSave(): void {
-    this.rollEntry.emit({rollValue: this.saveForm.get('roll')?.value, modifier : this.saveForm.get('modifier')?.value});
+    this.rollEntry.emit({rollValue: this.roll?.value, modifier : this.modifier?.value});
     this.activeModal.close('Close click')
   }
 
+  get roll() {
+    return this.saveForm.get('roll');
+  }
+
+  get modifier() {
+    return this.saveForm.get('modifier');
+  }
 }
