e27f4e8cecb36ac989a3a45343347c5e507ad9d9
mateonidas <teus.polec.other@gmail.com>
2022-06-03 14:11:59 +0200

    feat: add text from text.json for character-detail.component.html

--- src/app/character/character-detail/character-detail.component.ts
+++ src/app/character/character-detail/character-detail.component.ts
@@ -3,6 +3,7 @@ import {Character} from "../../model/character/character.model";
 import {CharacterService} from "../../shared/services/character-service/character.service";
 import {ActivatedRoute, Params, Router} from "@angular/router";
 import {SkirmishCharacterService} from "../../shared/services/skirmish-character-service/skirmish-character.service";
+import {TextResourceService} from "../../shared/services/text-resource-service/text-resource.service";
 
 @Component({
   selector: 'app-character-detail',
@@ -12,6 +13,7 @@ import {SkirmishCharacterService} from "../../shared/services/skirmish-character
 export class CharacterDetailComponent implements OnInit {
   character!: Character;
   private id!: number;
+  text = TextResourceService;
 
   constructor(public characterService: CharacterService,
               public skirmishService: SkirmishCharacterService,
