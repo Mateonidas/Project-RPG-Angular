5a880bacc649881864c3e69f4de5184f138e0ed7
mateonidas <teus.polec.other@gmail.com>
2022-07-17 23:02:07 +0200

    fix: fix for skill and talent lists fetching

--- src/app/shared/services/skill-service/skill.service.ts
+++ src/app/shared/services/skill-service/skill.service.ts
@@ -2,7 +2,6 @@ import {Injectable} from '@angular/core';
 import {Subject} from "rxjs";
 import {Skill} from "../../../model/skill/skill.model";
 import {HttpClient} from "@angular/common/http";
-import {tap} from "rxjs/operators";
 import {TextResourceService} from "../text-resource-service/text-resource.service";
 
 @Injectable({
@@ -18,14 +17,14 @@ export class SkillService {
   fetchSkills() {
     return this.http.get<Skill[]>('http://localhost:8080/skill').toPromise()
       .then(data => {
-          for (let skill of data) {
-            skill.nameTranslation = TextResourceService.getSkillNameTranslation(skill.name).nameTranslation;
-            this.skillList.push(skill);
-          }
-          this.skillList.sort(
-            (a, b) => (a.nameTranslation > b.nameTranslation) ? 1 : ((b.nameTranslation > a.nameTranslation) ? -1 : 0)
-          );
-          this.skillListChanged.next(this.skillList.slice());
-        });
+        for (let skill of data) {
+          skill.nameTranslation = TextResourceService.getSkillNameTranslation(skill.name).nameTranslation;
+        }
+        this.skillList = data;
+        this.skillList.sort(
+          (a, b) => (a.nameTranslation > b.nameTranslation) ? 1 : ((b.nameTranslation > a.nameTranslation) ? -1 : 0)
+        );
+        this.skillListChanged.next(this.skillList.slice());
+      });
   }
 }
--- src/app/shared/services/talent-service/talent.service.ts
+++ src/app/shared/services/talent-service/talent.service.ts
@@ -2,7 +2,6 @@ import {Injectable} from '@angular/core';
 import {Subject} from "rxjs";
 import {Talent} from "../../../model/talent/talent.model";
 import {HttpClient} from "@angular/common/http";
-import {tap} from "rxjs/operators";
 import {TextResourceService} from "../text-resource-service/text-resource.service";
 
 @Injectable({
@@ -18,14 +17,14 @@ export class TalentService {
   fetchTalent() {
     return this.http.get<Talent[]>('http://localhost:8080/talent').toPromise()
       .then(data => {
-          for (let talent of data) {
-            talent.nameTranslation = TextResourceService.getTalentNameTranslation(talent.name).nameTranslation;
-            this.talentList.push(talent);
-          }
-          this.talentList.sort(
-            (a, b) => (a.nameTranslation > b.nameTranslation) ? 1 : ((b.nameTranslation > a.nameTranslation) ? -1 : 0)
-          );
-          this.talentListChanged.next(this.talentList.slice());
-        });
+        for (let talent of data) {
+          talent.nameTranslation = TextResourceService.getTalentNameTranslation(talent.name).nameTranslation;
+        }
+        this.talentList = data;
+        this.talentList.sort(
+          (a, b) => (a.nameTranslation > b.nameTranslation) ? 1 : ((b.nameTranslation > a.nameTranslation) ? -1 : 0)
+        );
+        this.talentListChanged.next(this.talentList.slice());
+      });
   }
 }
