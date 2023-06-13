f2a0cc2d1329ddd46351645589207dfb29b0bc8c
mateonidas <teus.polec.other@gmail.com>
2023-06-13 11:58:58 +0200

    feat: add ignoring and destroying armor during receiving damages

--- src/app/dialog-window/receive-damage-dialog/receive-damage-dialog.component.ts
+++ src/app/dialog-window/receive-damage-dialog/receive-damage-dialog.component.ts
@@ -1,9 +1,9 @@
-import {Component, Inject, OnInit} from '@angular/core';
-import {TextResourceService} from "../../shared/services/text-resource-service/text-resource.service";
-import {MAT_DIALOG_DATA} from "@angular/material/dialog";
-import {ReceivedDamage} from "../../model/receive-damage/receive-damage.model";
-import {SkirmishCharacter} from "../../model/skirmish/skirmish-character.model";
-import {Model} from "../../model/model";
+import {Component, Inject, OnInit} from '@angular/core'
+import {TextResourceService} from "../../shared/services/text-resource-service/text-resource.service"
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog"
+import {ReceivedDamage} from "../../model/receive-damage/receive-damage.model"
+import {SkirmishCharacter} from "../../model/skirmish/skirmish-character.model"
+import {Model} from "../../model/model"
 
 @Component({
   selector: 'app-receive-damage-dialog',
@@ -12,20 +12,45 @@ import {Model} from "../../model/model";
 })
 export class ReceiveDamageDialog implements OnInit {
 
-  receivedDamage!: ReceivedDamage;
-  text = TextResourceService;
+  receivedDamage!: ReceivedDamage
+  isArmorDestroyed!: boolean
+  text = TextResourceService
 
-  constructor(@Inject(MAT_DIALOG_DATA) public character: SkirmishCharacter) {
+  constructor(@Inject(MAT_DIALOG_DATA) public character: SkirmishCharacter,
+              public dialogRef: MatDialogRef<ReceiveDamageDialog>) {
   }
 
   ngOnInit(): void {
-    this.receivedDamage = new ReceivedDamage(this.character.id);
-    this.receivedDamage.isWeaponUndamaging = false;
-    this.receivedDamage.isLosingTest = true;
-    this.receivedDamage.isSuddenDeath = true;
+    this.receivedDamage = new ReceivedDamage(
+      this.character.id,
+      false,
+      true,
+      true,
+      1,
+      false)
   }
 
   compareModels(c1: Model, c2: Model): boolean {
-    return c1 && c2 ? c1.name === c2.name : c1 === c2;
+    return c1 && c2 ? c1.name === c2.name : c1 === c2
+  }
+
+  saveAndCloseDialog() {
+    if(!this.receivedDamage.damage
+    || !this.receivedDamage.bodyLocalization) {
+      return
+    }
+
+    this.returnReceivedDamage().then((result) => {
+      this.dialogRef.close(result)
+    })
+  }
+
+  returnReceivedDamage(): Promise<ReceivedDamage> {
+    return new Promise((resolve => {
+      if(!this.isArmorDestroyed) {
+        this.receivedDamage.destroyArmorValue = 0
+      }
+      resolve(this.receivedDamage)
+    }))
   }
 }
--- src/app/model/receive-damage/receive-damage.model.ts
+++ src/app/model/receive-damage/receive-damage.model.ts
@@ -1,13 +1,21 @@
 export class ReceivedDamage {
-  public characterId: number;
-  public damage!: number;
-  public bodyLocalization!: string;
-  public isWeaponUndamaging!: boolean;
-  public isLosingTest!: boolean;
-  public shield!: number;
-  public isSuddenDeath!: boolean;
+  public characterId: number
+  public damage!: number
+  public bodyLocalization!: string
+  public isWeaponUndamaging!: boolean
+  public isLosingTest!: boolean
+  public shield!: number
+  public isSuddenDeath!: boolean
+  public destroyArmorValue!: number
+  public isIgnoringArmor!: boolean
 
-  constructor(characterId: number) {
-    this.characterId = characterId;
+
+  constructor(characterId: number, isWeaponUndamaging: boolean, isLosingTest: boolean, isSuddenDeath: boolean, destroyArmorValue: number, isIgnoringArmor: boolean) {
+    this.characterId = characterId
+    this.isWeaponUndamaging = isWeaponUndamaging
+    this.isLosingTest = isLosingTest
+    this.isSuddenDeath = isSuddenDeath
+    this.destroyArmorValue = destroyArmorValue
+    this.isIgnoringArmor = isIgnoringArmor
   }
 }
