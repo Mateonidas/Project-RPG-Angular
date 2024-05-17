f76d5517158362263264cfdcd337e4dbbcf6e4cc
mateonidas <teus.polec.other@gmail.com>
2024-05-17 14:56:28 +0200

    feat: change a location of a condition table in skirmish character details

--- src/app/details-components/details-skirmish-character-parameters/details-skirmish-character-parameters.component.ts
+++ src/app/details-components/details-skirmish-character-parameters/details-skirmish-character-parameters.component.ts
@@ -3,6 +3,11 @@ import {TextResourceService} from "../../shared/services/text-resource-service/t
 import {SkirmishCharacter} from "../../model/skirmish/skirmish-character.model";
 import {SkirmishCharacterService} from "../../shared/services/skirmish-character-service/skirmish-character.service";
 import {SkirmishService} from "../../shared/services/skirmish-service/skirmish.service";
+import {Model} from "../../model/model";
+import {
+  BottomSheetDescription
+} from "../../shared/bottom-sheet/bottom-sheet-description/bottom-sheet-description.component";
+import {MatBottomSheet} from "@angular/material/bottom-sheet";
 
 @Component({
   selector: 'app-details-skirmish-character-parameters',
@@ -13,9 +18,11 @@ export class DetailsSkirmishCharacterParametersComponent {
   text = TextResourceService
   @Input() skirmishCharacter!: SkirmishCharacter
   temporaryParametersColumns: string[] = ['isAlive', 'currentWounds', 'initiative', 'advantage']
+  baseColumns: string[] = ['name', 'level']
 
   constructor(public skirmishCharacterService: SkirmishCharacterService,
-              public skirmishService: SkirmishService) {
+              public skirmishService: SkirmishService,
+              protected bottomSheet: MatBottomSheet) {
   }
 
   async addAdvantagePoint() {
@@ -26,5 +33,12 @@ export class DetailsSkirmishCharacterParametersComponent {
   async removeAdvantagePoint() {
     await this.skirmishService.removeAdvantagePoint(this.skirmishCharacter.id)
     this.skirmishCharacter = await this.skirmishCharacterService.reloadSkirmishCharacter(this.skirmishCharacter.id)
+  }
+
+  openBottomSheet(model: Model) {
+    this.bottomSheet.open(BottomSheetDescription, {
+      data: {nameTranslation: model.nameTranslation, description: model.description},
+      panelClass: 'bottom-sheet'
+    })
   }
 }
