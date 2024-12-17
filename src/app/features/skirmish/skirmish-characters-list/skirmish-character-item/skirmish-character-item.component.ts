d2d7d31b67b93fffe17651e4fc225aeec0fee9b0
mateonidas <teus.polec.other@gmail.com>
2024-12-17 12:57:26 +0100

    feat: add health bar for skirmish character item

--- src/app/features/skirmish/skirmish-characters-list/skirmish-character-item/skirmish-character-item.component.ts
+++ src/app/features/skirmish/skirmish-characters-list/skirmish-character-item/skirmish-character-item.component.ts
@@ -13,9 +13,43 @@ export class SkirmishCharacterItemComponent implements OnInit {
 
   text = TextResourceService;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  getHealthBarStyle(): { [key: string]: string } {
+    const maxHealth = this.skirmishCharacter.character.wounds.value;
+    const currentHealth = this.skirmishCharacter.currentWounds;
+    const isDead = this.skirmishCharacter.isDead
+
+    let healthPercentage = 0
+
+    if (!isDead) {
+      healthPercentage = Math.max(0, Math.min(100, (currentHealth / maxHealth) * 100));
+    }
+
+    const startColor = {r: 22, g: 137, b: 55};
+    const endColor = {r: 244, g: 63, b: 94};
+    const {r, g, b} = this.calculateHealthColor(endColor, startColor, healthPercentage);
+    const a = 1;
+
+    return {
+      'background-color': `rgba(${r}, ${g}, ${b}, ${a})`,
+      'width': `${healthPercentage}%`,
+      'transition': 'width 0.3s ease, background-color 0.3s ease',
+    };
+  }
+
+  private calculateHealthColor(endColor: { r: number; g: number; b: number }, startColor: {
+    r: number;
+    g: number;
+    b: number
+  }, healthPercentage: number) {
+    const r = Math.round(endColor.r + (startColor.r - endColor.r) * (healthPercentage / 100));
+    const g = Math.round(endColor.g + (startColor.g - endColor.g) * (healthPercentage / 100));
+    const b = Math.round(endColor.b + (startColor.b - endColor.b) * (healthPercentage / 100));
+    return {r, g, b};
+  }
 }
